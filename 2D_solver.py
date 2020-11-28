56d7bcd902eacd8529fb210667a2a20c30582cb6
Juan Carbajal <juancarbajal98@gmail.com>
2020-11-28 03:33:52 -0800

    finished t-zone extraction

--- 2D_solver.py
+++ 2D_solver.py
@@ -1,24 +1,65 @@
+def getRow(index):
+    return int(index[0] / 3)
+def getColumn(index):
+    if((int(index[0]) % 3) != 0):
+        return ((int(index[0]) % 3)*3 + int(index[1]))
+    else:
+        return int(index[1])
+
+def getBox(index):
+    # first line of 3 boxes - options are box 0, 1, 2
+    if(getRow(index) < 3):
+        if(getColumn(index) < 3):
+            return 0
+        elif(getColumn(index) < 6):
+            return 1
+        else:
+            return 2
+    elif(getRow(index) < 6):
+        if(getColumn(index) < 3):
+            return 4
+        elif(getColumn(index) < 6):
+            return 5
+        else:
+            return 6
+    else:
+        if(getColumn(index) < 3):
+            return 7
+        elif(getColumn(index) < 6):
+            return 8
+        else:
+            return 9
 # should return 9 size array with data
 # @param - index:[a,b] where a ranges 0-27 and b 0-2
 # @param - data: 1d array of square objects
 def getRowData(index,data):
     rowData = []
-    rowNum = int(index[0] / 3)
-    print('index:'+str(index[0]) + ', ' + str(index[1]) +' row number:'+ str(rowNum))
+    rowNum = getRow(index)
+    #print('index:'+str(index[0]) + ', ' + str(index[1]) +' row number:'+ str(rowNum))
     rowData.append(data[(rowNum*9):(rowNum*9)+9])
-    for i in range(9):
-        print(rowData[0][i].value)
-    rowData = 
     return rowData
 
 def getColumnData(index,data):
-    ColumnData = []
-    print(index)
-    return ColumnData
+    columnData = []
+    temp = []
+    colNum = getColumn(index)
+    #print('index:'+str(index[0]) + ', ' + str(index[1]) +' column number:'+ str(colNum))
+    for l in range(9):
+        temp.append(data[l*9 + colNum])
+    columnData.append(temp)
+    return columnData
 
 def getBoxData(index,data):
     BoxData = []
-    print(index)
+    temp = []
+    boxNum = getBox(index)
+    for i in range(81):
+        # check index of every square 
+        # - find those with same boxNum
+        # - append these
+        if(getBox(data[i].index) == boxNum):
+            temp.append(data[i])
+    BoxData.append(temp)
     return BoxData
 
 class SuspectList:
@@ -78,14 +119,35 @@ class Grid:
 
     def trimSuspects(self,squares):
         for i in range(81):
-            t_zone = []
+            t_zone = {
+                "t_row":[],
+                "t_column":[],
+                "t_box":[]
+            }
+            t_row = []
+            t_column = []
+            t_box = []
             if(squares[i].suspectlist != None):
                 # pass in index and 1d squares data
-                # get back an array with all squares inside of this values row
-                getRowData(squares[i].index, squares)
-                #getColumnData(squares[i].index)
-                #getBoxData(squares[i].index)
+                # get back a 2d array with all squares inside of this values row in 0th index
+                rowData = getRowData(squares[i].index, squares)
+                for j in range(9):
+                    t_row.append(rowData[0][j].value)
+                t_zone["t_row"]=t_row
 
+                columnData = getColumnData(squares[i].index, squares)
+                for k in range(9):
+                    t_column.append(columnData[0][k].value)
+                t_zone["t_column"]=t_column
+                
+
+                boxData = getBoxData(squares[i].index, squares)
+                for i in range(9):
+                    t_box.append(boxData[0][i].value)
+                t_zone["t_box"] =t_box
+                print ('t_zone[t_row]:',t_zone["t_row"])
+                print ('t_zone[t_cal]:',t_zone["t_column"])
+                print ('t_zone[box]:',t_zone["t_box"])
 
 
 
