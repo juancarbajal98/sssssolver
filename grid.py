e39c70c8551ee592ae43737b9a5ae5e96a2ce772
Juan Carbajal <juancarbajal98@gmail.com>
2020-11-27 21:24:21 -0800

    WIP

--- grid.py
+++ grid.py
@@ -1,6 +1,26 @@
 # class structure - super being grid, sub being square
-
 import tkinter as Tk
+
+#import sys
+#sys.path.append('./packages')
+#from puzzle import Puzzle as p
+
+puzzle = [
+    [0, 0, 4], [0, 0, 0], [0, 6, 0], 
+    [0, 0, 0], [0, 0, 5], [0, 0, 0], 
+    [0, 1, 9], [2, 0, 6], [8, 0, 0], 
+    [0, 6, 0], [0, 7, 8], [0, 5, 9], 
+    [5, 0, 0], [0, 3, 0], [0, 0, 2], 
+    [1, 3, 0], [5, 9, 0], [0, 8, 0], 
+    [0, 0, 5], [7, 0, 1], [6, 4, 0], 
+    [0, 0, 0], [8, 0, 0], [0, 0, 0], 
+    [0, 2, 0], [0, 0, 0], [5, 0, 0]
+    ]
+
+#  0,0 0,1 0,2  1,0, 1,1 1,2  2,0 2,1 2,2
+#  3,0 3,1 3,2  4,0
+
+
 #import numpy as np
 class Grid(object):
     def __init__(self,root,n):
@@ -9,6 +29,7 @@ class Grid(object):
         # i.e. [[1,2],[3,4]]
         self.cubes = []
         self.current = (0,0)
+        global puzzle
         for i in range(n+1):
             for j in range(n+1):
                 game_board.append([i,j])
@@ -25,12 +46,11 @@ class Grid(object):
         for i in range(n):
             for j in range(n):
                 self.cubes.append(self.w.create_rectangle(cubeSize*game_board[j][1],cubeSize*game_board[i][1],cubeSize*game_board[j][1]+cubeSize,cubeSize*game_board[i][1]+cubeSize))
-        # set first square (top-left) to be orange
-        self.w.itemconfigure(self.cubes[0], fill = 'orange')
-        #paint every other square orange
-        #for i in range(0,n*n+1,2):
-            #self.w.itemconfigure(cubes[i], fill = 'orange')
-        #self.w.bind('<Button-1>',self.squareclick)
+        # fill in numbers
+        #for i in range(27):
+          #  for j in range(3):
+                #print(puzzle[i][j])
+                #self.w.create_text((cubeSize*i, cubeSize*i+100), text = str(puzzle[i][j]))
         self.w.pack()
 
 def knights_tour(n):
@@ -40,6 +60,3 @@ def knights_tour(n):
 if __name__=='__main__':
     knights_tour(9)
 
-
-
-#mat = [	0, 0, 4, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 1, 9, 2, 0, 6, 8, 0, 0, 0, 6, 0, 0, 7, 8, 0, 5, 9, 5, 0, 0, 0, 3, 0, 0, 0, 2, 1, 3, 0, 5, 9, 0, 0, 8, 0, 0, 0, 5, 7, 0, 1, 6, 4, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 5, 0, 0]
