501bfcb9c391f372c76ce8b19fd8bde012eef639
deanthebeandip <deanthebean@g.ucla.edu>
2020-11-28 16:42:51 +0800

    worked on 1D,  working on sus list

--- 1D_solver.py
+++ 1D_solver.py
@@ -9,6 +9,7 @@ def mat_print(in_mat):
 			print(' ')
 	print(' ')
 
+
 def row_print(in_mat, num):
 	for i in range(len(in_mat)):
 		if ((num-1)*9 <= i) and (i < (num)*9):
@@ -16,12 +17,14 @@ def row_print(in_mat, num):
 	print(' ')
 	print(' ')
 
+
 #0, 9, 18, 27...
 def col_print(in_mat, num):
 	for i in range(len(in_mat)):
 		if (i % 9) == num-1:
 			print(in_mat[i])
 	print(' ')
+
 
 def box_print(in_mat, num):
 	step = 0
@@ -70,10 +73,18 @@ def box_grab(in_mat, num):
 			step = 0
 	return out_mat
 
-def checksum(in_mat):
+def checksum(in_mat, op, num):
+	temp_mat = []
+	if(op == 0):
+		temp_mat = row_grab(in_mat, num)
+	elif(op == 1):
+		temp_mat = col_grab(in_mat, num)
+	elif(op == 2):
+		temp_mat = box_grab(in_mat, num)
+
 	sum = 0
-	for i in range(len(in_mat)):
-		sum += in_mat[i]
+	for i in range(len(temp_mat)):
+		sum += temp_mat[i]
 
 	if(sum == 45):
 		return 1
@@ -86,10 +97,13 @@ class Grid:
 	def __init__(self, data):
 		self.data = data
 		self.squares = []
+		self.suspectList = None
 
 	def populate(self):
 		for i in range(len(self.data)):
 			s = Squares(i, self.data[i])
+			if(s.value == 0):
+				s.suspectList = []
 			self.squares.append(s)
 
 	def print(self):
@@ -100,11 +114,36 @@ class Grid:
 
 	def package(self):
 		out_mat = []
-
-		for i in range(self.data):
-			out_mat[i] = self.data[i]
-
+		for i in range(len(self.data)):
+			out_mat.append(self.data[i])
 		return out_mat
+
+	def suslist(self):
+		for i in range(len(self.data)):
+			sus_temp = []
+			if self.data[i] == 0:
+				#check row, then column, then box
+				#in the future, keep the row temp since we on the same one...
+				sus_temp = row_grab(self.package(), int(i/9)+1)
+
+				for j in range(len(sus_temp)):
+					print(sus_temp[j], end = " ")
+				print(" ")
+
+				sus_temp = col_grab(self.package(), (i+1) % 9)
+				for j in range(len(sus_temp)):
+					print(sus_temp[j], end = " ")
+				print(" ")
+
+
+				sus_temp = box_grab(self.package(), int(i/27)*3 + (int(i/3)%3)+1)
+				for j in range(len(sus_temp)):
+					print(sus_temp[j], end = " ")
+				print(" ")
+
+
+			print(" ")
+
 
 
 class Squares:
@@ -113,16 +152,11 @@ class Squares:
 		self.value = value
 		self.suspectList = []
 
-	def addsus()
-
-
-
-
-
-
 
 ###########TESTING############
 mat = [	0, 0, 4, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 1, 9, 2, 0, 6, 8, 0, 0, 0, 6, 0, 0, 7, 8, 0, 5, 9, 5, 0, 0, 0, 3, 0, 0, 0, 2, 1, 3, 0, 5, 9, 0, 0, 8, 0, 0, 0, 5, 7, 0, 1, 6, 4, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 5, 0, 0]
 g = Grid(mat)
-g.populate()
-g.print()
+#row_print(g.package(), 5)
+
+mat_print(g.package())
+g.suslist()
